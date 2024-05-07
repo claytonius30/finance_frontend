cd1f62230c4ce88030aea5df754a9508ffb2f671
claytonius30 <cjdesimone30@gmail.com>
2024-05-07 10:43:21 -0600

    added FinanaceMAUI.Tests project within NETfinalMAUI root folder. Placed FinanceMAUI within own folder

---
+++ FinanceMAUI.Tests/ViewModels/UserDetailViewModelTests.cs
@@ -0,0 +1,58 @@
+﻿using FinanceMAUI.Services;
+using FinanceMAUI.ViewModels;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceMAUI.Tests.ViewModels
+{
+    public class UserDetailViewModelTests
+    {
+        [Fact]
+        public async Task UserDetailWithId_IsInitialized_GetUserIsCalled()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+
+            var userService = Substitute.For<IUserService>();
+            var navigationService = Substitute.For<INavigationService>();
+            var dialogService = Substitute.For<IDialogService>();
+
+            var sut = new UserDetailViewModel(userService, navigationService, dialogService);
+            sut.Id = id;
+
+            // Act
+            await sut.LoadAsync();
+
+            // Assert
+            await userService
+                .Received(1)
+                .GetUser(id);
+        }
+
+        [Fact]
+        public async Task UserDetailWithGuidEmptyId_IsInitialized_GetUserIsNotCalled()
+        {
+            // Arrange
+            var id = Guid.Empty;
+
+            var eventService = Substitute.For<IUserService>();
+            var navigationService = Substitute.For<INavigationService>();
+            var dialogService = Substitute.For<IDialogService>();
+
+            var sut = new UserDetailViewModel(eventService, navigationService, dialogService);
+            sut.Id = id;
+
+            // Act
+            await sut.LoadAsync();
+
+            // Assert
+            await eventService
+                .DidNotReceive()
+                .GetUser(Arg.Any<Guid>());
+        }
+    }
+}
